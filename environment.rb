a097169825d3966ae19ad506e9bd4cd89949a38c
Adrien Husson <pangel.neu@gmail.com>
2009-05-14 10:52:57 -0700

    Load path now include lib/

--- environment.rb
+++ environment.rb
@@ -11,9 +11,9 @@ configure do
   PROJECT_NAME = "collections"
   
   # Load extensions
-  # $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
+  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   # Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| load File.basename(lib, '.*') }
-  require "lib/collectionreader"
+  require "collectionreader"
 end
 
 configure :development do end
