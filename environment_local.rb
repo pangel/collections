b43e2f9e2fdd2efabf675e66a2c33eedb682c2a6
Adrien Husson <pangel.neu@gmail.com>
2009-11-15 19:18:57 -0800

    Removed useless dependencies to Redis libraries.

--- environment_local.rb
+++ environment_local.rb
@@ -8,12 +8,6 @@ configure do
 
   set :root, File.dirname(__FILE__)
   set :public, Proc.new { File.join(root, "public") }
-
-  # Load extensions
-  $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
-  require "loader"
-  require "reader"
-  require "redisdb"
 end
 
 configure :development do end
